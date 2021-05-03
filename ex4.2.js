dfbab0b368a7f09e1bfb13dcb5d30fa1c8ba63b2
Milton Castro <castro.milton07@gmail.com>
2021-05-02 23:55:46 -0300

    02 - Adiciona o arquivo ex4.2.js com o incremento dos exercicios 04, 05 e 06

--- ex4.2.js
+++ ex4.2.js
@@ -19,3 +19,28 @@ for (let number of numbers) {
 }
 average = (average/numbers.length)
 console.log(average);
+
+// Ex. 04
+if (average > 20) {
+    console.log('valor maior que 20');
+} else console.log('valor menor ou igual a 20');
+
+// Ex. 05
+let higherNumber = 0;
+for (let number of numbers) {
+    if (number > higherNumber) {
+        higherNumber = number;
+    }
+}
+console.log(higherNumber);
+
+// Ex. 06
+let oddCount = 0;
+for (let number of numbers) {
+    if (number % 2 !== 0) {
+        oddCount += 1;
+    }
+}
+if (oddCount < 1) {
+    console.log('nenhum valor ímpar encontrado');
+} else console.log(oddCount);

1c6d01055aabe6bd3802e8595ec9a788b4fd3fb5
Milton Castro <castro.milton07@gmail.com>
2021-05-26 01:51:03 -0300

    03 - BONUS: Exercicios 1, 2, 3 e 4 da Parte 2 (incompleto

--- bonus.js
+++ bonus.js
@@ -22,17 +22,17 @@ const battleMembers = { mage, warrior, dragon };
 
 // Parte 1
 
-const getDragonDamage = (dragon) => {
+const getDragonDamage = () => {
     const dragonDamage = Math.floor(Math.random() * (dragon.strength - 14)) + 15;
     return dragonDamage
 };
 
-const getWarriorDamage = (warrior) => {
+const getWarriorDamage = () => {
     const warriorDamage = Math.floor(Math.random() * ((warrior.strength * warrior.weaponDmg) - (warrior.strength - 1))) + warrior.strength;
     return warriorDamage;
 };
 
-const getMageDamage = (mage) => {
+const getMageDamage = () => {
     const mageDamageAndMana = {};
     mageDamageAndMana.damage = Math.floor(Math.random() * ((mage.intelligence * 2) - (mage.intelligence - 1))) + mage.intelligence;
     if (mage.mana < 15) mageDamageAndMana.damage = 'Não possui mana suficiente';
@@ -42,3 +42,26 @@ const getMageDamage = (mage) => {
 
 // Parte 2
 
+const gameActions = {
+    warriorAction: (callback) => {
+        const damage = callback();
+        dragon.healthPoints -= damage
+        warrior.damage = damage;
+    },
+    mageAction: (callback) => {
+        const damageAndMana = callback();
+        dragon.healthPoints -= damageAndMana.damage;
+        mage.damage = damageAndMana.damage;
+        mage.mana = damageAndMana.mana;
+    },
+    dragonAction: (callback) => {
+        const damage = callback();
+        mage.healthPoints -= damage;
+        warrior.healthPoints -= damage;
+        dragon.damage = damage;
+    },
+    fetchBattleMambers: () => {
+        console.log(battleMembers);
+        return battleMembers;
+    },
+};

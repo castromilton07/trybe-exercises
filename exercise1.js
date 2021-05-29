94179aee16e8b76daa3c0e2ffdafa56e6fe0886d
Milton Castro <castro.milton07@gmail.com>
2021-05-29 00:17:08 -0300

    Exercicio 1

--- exercise1.js
+++ exercise1.js
@@ -6,8 +6,12 @@ const arrays = [
   [4, 5, 6],
 ];
 
-function flatten() {
-  // escreva seu código aqui
-}
+const flatten = () => arrays.reduce((previousValue, currentValue) => previousValue.concat(currentValue));
+
+/* const flatten = () => arrays.reduce((previousValue, currentValue) => {
+    const arr = previousValue;
+    currentValue.forEach((value) => arr.push(value));
+    return arr;
+}); */
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
